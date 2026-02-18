2586a77881857c0b679be80864225def0352276c
JayoungKim <kjy95960125@gmail.com>
2026-02-18 20:20:46 +0900

    url변경

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   const [message, setMessage] = useState("");
   useEffect(() => {
     async function getData() {
-      const response = await fetch("http://127.0.0.1:8000");
+      const response = await fetch(`${import.meta.env.VITE_API_URL}`);
       const data = await response.json();
       setMessage(data);
     }
